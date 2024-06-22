94ffd22444670567e95dcb018014e99ce0af0f04
Ayush Kumar Shaw <hitme@yourakshaw.com>
2024-06-22 07:16:46 +0000

    feat(class): add third program file for day 2

---
+++ Day02/Assignment.java
@@ -0,0 +1,23 @@
+public class Assignment {
+    public static void main(String[] args) {
+
+        byte a = 5;
+        short b = 5;
+        int c = 5;
+        long d = 5;
+
+        float e = 5;
+        double f = 5;
+
+        boolean g = true;
+
+        char ch = 'A';
+
+        // ASCII - American Standard Code for Information Interchange
+        // ASCII range for digits: 0 (48) - 9 (57)
+        // ASCII range for lower case alphabets: 97 ('a') - 122 ('z')
+        // ASCII range for upper case alphabets: 65 ('A') = 90 ('Z')
+
+        System.out.println("The ASCII value for 'A' is " + (int) ch);
+    }
+}
