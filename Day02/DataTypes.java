18cfea2030da4a7ef75f33f9c9714db1b41830a9
Ayush Kumar Shaw <hitme@yourakshaw.com>
2024-06-22 06:58:36 +0000

    fix(class): update DataTypes.java

--- Day02/DataTypes.java
+++ Day02/DataTypes.java
@@ -3,12 +3,14 @@ public class DataTypes {
         
         System.out.println("These are the primitive data types:");
         System.out.println();
-        System.out.println("1. int | Integer");
-        System.out.println("2. long | Long Integer");
-        System.out.println("3. float | Real Number");
-        System.out.println("4. double | Long Real Number");
-        System.out.println("5. boolean | Boolean (true or false)");
-        System.out.println("6. char | Character");
+        System.out.println("1. byte | Byte Integer");
+        System.out.println("2. short | Short Integer");
+        System.out.println("3. int | Integer");
+        System.out.println("4. long | Long Integer");
+        System.out.println("5. float | Real Number");
+        System.out.println("6. double | Long Real Number");
+        System.out.println("7. boolean | Boolean (true or false)");
+        System.out.println("8. char | Character");
 
         System.out.println();
         System.out.println("These are the comment non-primitive data types:");
