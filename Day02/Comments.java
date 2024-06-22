cf7e810555028146bfac3f2b60391e0b13607bf8
Ayush Kumar Shaw <hitme@yourakshaw.com>
2024-06-22 06:43:20 +0000

    feat(class): add first program file for day 2

---
+++ Day02/Comments.java
@@ -0,0 +1,28 @@
+public class Comments {
+    public static void main(String[] args) {
+        System.out.println("Java has 3 types of comments:");
+        System.out.println();
+
+        // This is a single line comment
+        System.out.println("1. Single line comment");
+
+        /*
+         * This
+         * is
+         * a
+         * multi
+         * line
+         * comment
+         */
+        System.out.println("2. Multi line comment");
+
+        /**
+         * This
+         * is
+         * a 
+         * documentation
+         * comment
+         */
+        System.out.println("3. Documentation comment");
+    }
+}
