1c7aed5565c3ec1d968079cf8e71cc6d2a22dddf
Ayush Kumar Shaw <hitme@yourakshaw.com>
2024-06-22 06:54:45 +0000

    feat(class): add second program file for day 2

---
+++ Day02/DataTypes.java
@@ -0,0 +1,19 @@
+public class DataTypes {
+    public static void main(String[] args) {
+        
+        System.out.println("These are the primitive data types:");
+        System.out.println();
+        System.out.println("1. int | Integer");
+        System.out.println("2. long | Long Integer");
+        System.out.println("3. float | Real Number");
+        System.out.println("4. double | Long Real Number");
+        System.out.println("5. boolean | Boolean (true or false)");
+        System.out.println("6. char | Character");
+
+        System.out.println();
+        System.out.println("These are the comment non-primitive data types:");
+        System.out.println();
+        System.out.println("1. String | Collection of characters");
+        System.out.println("2. Array | Collection of values of the same data type");
+    }
+}
