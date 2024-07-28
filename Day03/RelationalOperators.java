d95876ab9993bdf876a5ed9fc1d5cbec56563e0e
Ayush Kumar Shaw <hitme@yourakshaw.com>
2024-07-28 13:29:40 +0000

    add second program for Day03

---
+++ Day03/RelationalOperators.java
@@ -0,0 +1,62 @@
+/*
+# Relational Operators
+
+# Relational Operators are used for finding 
+# the relationship between two values on the 
+# basis of their values. The return type for a 
+# relational operation is boolean, i.e., it returns 
+# True or False.
+
+# Realation Operators in Python
+
+# <		-- less than
+# >		-- greater than
+# <=	-- less than or equal to
+# >=	-- greater than or equal to
+# ==	-- equality
+# !=	-- not equal
+
+print("5 < 6 is", 5 < 6)
+print("5 > 6 is", 5 > 6)
+print("5 <= 6 is", 5 <= 6)
+print("5 >= 6 is", 5 >= 6)
+print("5 == 6 is", 5 == 6)
+print("5 != 6 is", 5 != 6)
+print()
+print("Distinction between < and <= :")
+print("6 < 6 is", 6 < 6)
+print("6 <= 6 is", 6 <= 6)
+*/
+
+// Relational Operators
+
+// Relational Operators are used for finding 
+// the relationship between two values on the 
+// basis of their values. The return type for a 
+// relational operation is boolean, i.e., it returns 
+// true or false.
+
+// Realation Operators in Java
+
+// <	-- less than
+// >	-- greater than
+// <=	-- less than or equal to
+// >=	-- greater than or equal to
+// ==	-- equality
+// !=	-- not equal
+
+public class RelationalOperators {
+    public static void main(String[] args) {
+
+        System.out.println("5 < 6 is " + (5 < 6));
+        System.out.println("5 > 6 is " + (5 > 6));
+        System.out.println("5 <= 6 is " + (5 <= 6));
+        System.out.println("5 >= 6 is " + (5 >= 6));
+        System.out.println("5 == 6 is " + (5 == 6));
+        System.out.println("5 != 6 is " + (5 != 6));
+        System.out.println();
+        System.out.println("Distinction between < and <= :");
+        System.out.println("6 < 6 is " + (6 < 6));
+        System.out.println("6 <= 6 is " + (6 <= 6));
+    }
+}
